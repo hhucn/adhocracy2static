6bf0a284766947372783b14906ef38e06ffec382
Alina Barendt <alina@praktikum>
2015-06-19 10:25:49 +0200

    erweiterung abspeichern von url

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-import urllib.request
+import re, urllib.request
+from urllib.parse import urljoin
 
-#lädt den Quellcode und speichert in in einer Datei
-website = "https://normsetzung.cs.uni-duesseldorf.de"
-t = urllib.request.urlopen(website)
-f = open ("website.txt", "w")
-f.write(str(t.read()))
+ 
+b = "https://normsetzung.cs.uni-duesseldorf.de"
+s = urllib.request.urlopen(b).read().decode('utf-8')
+ 
+def get_a(b, s):
+    for m in re.finditer(r"""(?is)<a[^>]*href[\s\n]*=[\s\n]*("[^"]*"|'[^']*')""", s):
+        yield urljoin(b, m.group(1)[1:-1])
+ 
+s= list(get_a(b, s))
+#print(s)
+x=0
+file = open("test.txt", "w")
+
+while x < len(s):   
+        site=urllib.request.urlopen(s[x])
+        a=site.read()
+        file.write(str(a))
+        x+=1
+file.close()
+ 
