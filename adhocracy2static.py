d325e21797e1d0d463e8626a2032fc621bcbf2c0
maride <martin.dessauer@hhu.de>
2015-12-02 15:22:18 +0100

    Remove protocol prefix

--- adhocracy2static.py
+++ adhocracy2static.py
@@ -1,7 +1,7 @@
 import re, urllib.request
 from urllib.parse import urljoin
 
- 
+
 base_url = "https://normsetzung.cs.uni-duesseldorf.de"
 
 def main():
@@ -14,20 +14,21 @@ def main():
     links.remove(links[0])
     print(links)
     print (len(links))
-    for link in links: 
-            l=link
-            dateiname=l.replace("/","")
+
+    for link in links:
+            dateiname = link.replace("/","")
+            dateiname = dateiname.replace("https", "")
+            dateiname = dateiname.replace("http", "")
+
             with open(dateiname+".txt", "wb") as f:
                 site=urllib.request.urlopen(link)
                 site_content = site.read()
                 f.write(site_content)
                 x+=1
-                
-      
-  
+
 def find_links(base_url, s):
     for m in re.finditer(r"""(?is)<a[^>]*href[\s\n]*=[\s\n]*("[^"]*"|'[^']*')""", s):
         yield urljoin(base_url, m.group(1)[1:-1])
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
